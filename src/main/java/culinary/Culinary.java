c4f9c386e004964c12ad27000f5d88464955dbea
Olya Vasilkovich <voa77777@gmail.com>
2017-02-14 12:44:00 +0200

    Changed

--- src/main/java/culinary/Culinary.java
+++ src/main/java/culinary/Culinary.java
@@ -6,13 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 /**
  * Created by olya on 10.02.2017.
  */
-
+@PropertySource("application.properties")
 @Configuration
 @SpringBootApplication
 @EnableSpringDataWebSupport
